373945ca6dc1e45f24681fc8a02991b54da39b85
JHkim95 <36726912+JHkim95@users.noreply.github.com>
2018-09-21 15:02:40 +0900

    Update Smain2.py

--- Recent/Smain2.py
+++ Recent/Smain2.py
@@ -2,6 +2,7 @@ import servo_test
 import motor
 import socket
 import time
+import sys
 
 def init():
     motor.setup()
@@ -9,8 +10,8 @@ def init():
     motor.setSpeed(0)
     servo_test.pwm.write(0,0,500)
 
-HOST = "192.168.1.103"
-PORT = 5077
+HOST = "192.168.0.6"
+PORT = 5333
 BUFSIZE = 2
 ADDR = (HOST, PORT)
 
@@ -20,62 +21,64 @@ ssock.listen(0)
 csock = None
 
 init()
+try :
+	while True:
+		if csock is None:
+			print("Waiting for connection...")
+			csock, addr_info = ssock.accept()
+			print("Get connection from", addr_info)
+		else:
+			total_data = csock.recv(BUFSIZE)
+			total_data = total_data.decode()
 
-while True:
+			forward = total_data[0]
+			direction = total_data[1]
 
-	if csock is None:
-		print("waiting for connection...")
-		csock, addr_info = ssock.accept()
-		print("got connection from", addr_info)
-	else:
-		total_data = csock.recv(BUFSIZE)
-		total_data = total_data.decode()
-#		print(str(total_data))
+			if (forward == "3") :
+				speed = 35
+				motor.setSpeed(speed)
+				motor.forward()
 
-		forward = total_data[0]
-		
-		direction = total_data[1]
-	
+			elif (forward == "2") :
+				speed = 25
+				motor.setSpeed(speed)
+				motor.forward()
 
-		if (forward == "3") :   
-			speed = 30
-			motor.setSpeed(speed)
-			motor.forward()
+			elif (forward =="1"):
+				speed = 25
+				motor.setSpeed(speed)
+				motor.backward()
 
-		elif (forward == "2") :   
-			speed = 20
-			motor.setSpeed(speed)
-			motor.forward()
+			elif (forward == "0") :
+				speed = 30
+				motor.setSpeed(speed)
+				motor.backward()
 
-		elif (forward =="1"):
-			speed = 20
-			motor.setSpeed(speed)
-			motor.backward()
+			elif forward == "4":
+				motor.stop()
 
-		elif (forward == "0") :   
-			speed = 30
-			motor.setSpeed(speed)
-			motor.backward()
+			elif forward == "5":
+				motor.stop()
 
-		elif forward == "4":
-			motor.stop()
-		
-		elif forward == "5":
-			motor.stop()
 
-		if direction == "0":
-			servo_test.pwm.write(0, 0, 330)
+				
+			if direction == "0":
+				servo_test.pwm.write(0, 0, 330)
 
-		elif direction == "1":
-			servo_test.pwm.write(0, 0, 400)
+			elif direction == "1":
+				servo_test.pwm.write(0, 0, 400)
 
-		elif direction == "2":
-			servo_test.pwm.write(0, 0, 500)
+			elif direction == "2":
+				servo_test.pwm.write(0, 0, 500)
 
-		elif direction == "3":
-			servo_test.pwm.write(0, 0, 550)
+			elif direction == "3":
+				servo_test.pwm.write(0, 0, 550)
 
-		elif direction == "4":
-			servo_test.pwm.write(0, 0, 650)
-		
-		
+			elif direction == "4":
+				servo_test.pwm.write(0, 0, 700)
+
+except KeyboardInterrupt:
+	pass
+
+print("\n STOP!")
+sys.exit(1)
